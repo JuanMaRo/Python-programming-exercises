0fb4c19ebf9c514107f1fc536a56bc8b8d58f305
Juan Márquez <marquez.cucho@gmail.com>
2018-06-13 12:48:21 -0500

    Ejercicio 5

--- q5.py
+++ q5.py
@@ -1,2 +1,15 @@
+class Estudiante(object):
+    def __init__(self):
+        self.nombre = ''
+
+    def getString(self):
+        self.nombre = str(input('Tu nombre es '))
+
+    def printString(self):
+        return self.nombre.upper()
+
+
 if __name__ == '__main__':
-    run()
+    e = Estudiante()
+    e.getString()
+    print(e.printString())
