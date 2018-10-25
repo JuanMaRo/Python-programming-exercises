5d18a455d83c92b13302b29fe565b292bc70a33b
Juan Márquez <marquez.cucho@gmail.com>
2018-10-24 23:58:46 -0500

    the exercise is incomplete

---
+++ q021.py
@@ -0,0 +1,14 @@
+import math
+
+
+def run():
+    ecuacion = math.sqrt(((5-3)**2) + ((3-2)**2))
+    print(int(ecuacion))
+
+
+if __name__ == '__main__':
+    run()
+
+#t = 12345, 54321, 'Tom'
+#type(t)
+#tuple
