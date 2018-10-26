f02b4619ec59435b9405f654269ba7b4a4f2167f
Juan Márquez <marquez.cucho@gmail.com>
2018-10-26 18:41:41 -0500

    exercise 21

--- q021.py
+++ q021.py
@@ -1,13 +1,17 @@
 import math
 
 
-def run():
-    ecuacion = math.sqrt(((5-3)**2) + ((3-2)**2))
+def run(x1,x2,y1,y2):
+    ecuacion = math.sqrt(((x1-x2)**2) + ((y1-y2)**2))
     print(int(ecuacion))
 
 
 if __name__ == '__main__':
-    run()
+    x1 = int(input('UP '))
+    x2 = int(input('DOWN '))
+    y1 = int(input('LEFT '))
+    y2 = int(input('RIGHT '))
+    run(x1,x2,y1,y2)
 
 #t = 12345, 54321, 'Tom'
 #type(t)
